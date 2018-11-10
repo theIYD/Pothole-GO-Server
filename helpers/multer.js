afcd2033207d1e85699c7d7b8491c465ac206a81
Idrees Dargahwala <idrees@theiyd.com>
2018-11-10 08:05:25 +0530

    Refactor the multer functions

---
+++ helpers/multer.js
@@ -0,0 +1,34 @@
+const multer = require("multer");
+
+// Multer Functions
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
+
+module.exports = {
+    storage: storage,
+    fileFilter: fileFilter,
+    upload: upload
+}
