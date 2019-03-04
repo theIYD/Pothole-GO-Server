515faf31064966a5cf66eadb77a704db5cd744ae
Idrees Dargahwala <idrees@theiyd.com>
2019-03-04 14:12:54 +0530

    admin routes

--- routes/Login.js
+++ routes/Login.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const loginController = require("../controllers/login");
 
+// User login
 router.post("/login", loginController.loginUser);
+
+// Admin login
+router.post("/admin/register", loginController.registerAdmin);
+router.post("/admin", loginController.adminLogin);
 
 module.exports = router;
